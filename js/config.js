c8e2d311ff9753122646492a2fdc58aa8c167547
yusril <m.yusril.al.ubadillah@gmail.com>
2026-07-29 16:46:18 +0700

    perubahan backend untuk logika tidak double dr

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ==========================================
 // KONFIGURASI APLIKASI
 // ==========================================
-const API_URL = "https://script.google.com/macros/s/AKfycbwLRdHXEtV6T5VWk3osebCqg3ZEghALroja4FakDpAk3V7N3H6IdjdQqfGmW7IpuhNh/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbzMc2VdKydEENw0RUPNU07kwxWTPFn3ShnBM-RAsP_qYfdvZ_a6y08TsI-NoqCCoKs0/exec"; 
 const EMAIL_API_URL = "https://script.google.com/macros/s/AKfycbzYZyDvtoPJTZlPtt-8PGxXOYxBrnknUemODJK3DTMQ7QEjHrAeu08slaKZ6bgbNlN5/exec";
 const NAME_CORP = "PT SINARAYA NUGRAHA AHMADARIS MEDIKA";
 
