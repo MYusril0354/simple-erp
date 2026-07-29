aa7bcd918971fdcaf8f60acdcb96ed97c8266bd3
yusril <m.yusril.al.ubadillah@gmail.com>
2026-07-29 08:48:26 +0700

    backend change perihal perhitungan dr semua dr meskipun yang ada Sj nya

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ==========================================
 // KONFIGURASI APLIKASI
 // ==========================================
-const API_URL = "https://script.google.com/macros/s/AKfycbw0gm-Yw8EKqLlwyRQdyjD-T5gtgrJCnCtldn8vNQBmp-G9AnhptJEInzvWd5oxCgzU/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbxWqHFYQVibhjzlKagyCKyAyiq44lntR9P8G8idNOfwVj-Gwe0OfpnQXBER8o8w1Top/exec"; 
 const EMAIL_API_URL = "https://script.google.com/macros/s/AKfycbzYZyDvtoPJTZlPtt-8PGxXOYxBrnknUemODJK3DTMQ7QEjHrAeu08slaKZ6bgbNlN5/exec";
 const NAME_CORP = "PT SINARAYA NUGRAHA AHMADARIS MEDIKA";
 
