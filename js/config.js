6a893eae9e6029fa337b9a081dac18d943ffaf49
yusril <m.yusril.al.ubadillah@gmail.com>
2026-07-29 16:25:54 +0700

    perubahan backend untuk logika persiapan barang meskipun disitu parsial tetap tidak hilang di persiapan barang atau status po masih open

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ==========================================
 // KONFIGURASI APLIKASI
 // ==========================================
-const API_URL = "https://script.google.com/macros/s/AKfycbxWqHFYQVibhjzlKagyCKyAyiq44lntR9P8G8idNOfwVj-Gwe0OfpnQXBER8o8w1Top/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbwLRdHXEtV6T5VWk3osebCqg3ZEghALroja4FakDpAk3V7N3H6IdjdQqfGmW7IpuhNh/exec"; 
 const EMAIL_API_URL = "https://script.google.com/macros/s/AKfycbzYZyDvtoPJTZlPtt-8PGxXOYxBrnknUemODJK3DTMQ7QEjHrAeu08slaKZ6bgbNlN5/exec";
 const NAME_CORP = "PT SINARAYA NUGRAHA AHMADARIS MEDIKA";
 
