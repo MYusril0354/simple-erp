a614dcff90a1b2d7d5c568a638c0ed7e1c5ca0c1
yusril <m.yusril.al.ubadillah@gmail.com>
2026-07-30 07:14:49 +0700

    pembaruan logika apigetdelivery alokasi agar tidak men dr 10 padahal tanpa alokasi 5

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ==========================================
 // KONFIGURASI APLIKASI
 // ==========================================
-const API_URL = "https://script.google.com/macros/s/AKfycbykX4dlkktjtGKG8JCs_sBARZwKHV2b1HGaIb3oadYY1U1ZbhdsBY6A7mfIZfqRlJhd/exec"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbxdKjCLZFtVzRJRL5aU0_OoV4WFZum9doEbVzxIuyoB0XNLSK7UTqIP828Q9G9_3VE/exec"; 
 const EMAIL_API_URL = "https://script.google.com/macros/s/AKfycbzYZyDvtoPJTZlPtt-8PGxXOYxBrnknUemODJK3DTMQ7QEjHrAeu08slaKZ6bgbNlN5/exec";
 const NAME_CORP = "PT SINARAYA NUGRAHA AHMADARIS MEDIKA";
 const ADDRESS_CORP = "Pandaan"
