0fb7038b8af9c1f3a44005df89ad8e83eb2225ad
yusril <m.yusril.al.ubadillah@gmail.com>
2026-08-04 19:41:48 +0700

    penghapusan nama dan alamat corporasi

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ==========================================
 const API_URL = "https://script.google.com/macros/s/AKfycbxdKjCLZFtVzRJRL5aU0_OoV4WFZum9doEbVzxIuyoB0XNLSK7UTqIP828Q9G9_3VE/exec"; 
 const EMAIL_API_URL = "https://script.google.com/macros/s/AKfycbzYZyDvtoPJTZlPtt-8PGxXOYxBrnknUemODJK3DTMQ7QEjHrAeu08slaKZ6bgbNlN5/exec";
-const NAME_CORP = "PT SINARAYA NUGRAHA AHMADARIS MEDIKA";
-const ADDRESS_CORP = "Pandaan"
+const NAME_CORP = " ";
+const ADDRESS_CORP = " "
 
 let currentUser = null;
